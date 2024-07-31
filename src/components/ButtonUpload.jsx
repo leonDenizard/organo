4f5bee3e7971a65130905b8e2fbcf9201b27614d
leonDenizard <de.nizard@hotmail.com>
2024-07-30 23:40:41 -0300

    Style: Estilizando tela de registro do usuário na plataforma

--- src/components/ButtonUpload.jsx
+++ src/components/ButtonUpload.jsx
@@ -2,11 +2,12 @@ export default function ButtonUpload() {
     return (
         <label className="block">
             <input type="file" className="block w-full text-sm text-slate-300 mt-4
-                file:mr-4 file:py-2 file:px-4
+                file:mr-4 file:py-3 file:px-4
                 file:rounded-full file:border-0
                 file:text-sm file:font-semibold
                 file:bg-bubble-red file:text-white
-                hover:file:bg-bubble-red/85
+                hover:file:bg-bubble-red/85 
+                file:shadow-shadow-button-upload                
                 file:cursor-pointer
                 file:transition-colors file:300ms
                 "/>
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -17,14 +17,14 @@ export default function Register() {
     setSelectedRole(selectedRole === id ? null : id)
   }
 
-  const handleChangeManager = (id) =>{
+  const handleChangeManager = (id) => {
     setSelectedManager(selectedManager === id ? null : id)
   }
   return (
-    <form className="relative min-h-screen flex items-center">
-      <div className="border-2 border-border-color w-[90%] h-[600px] m-auto">
+    <form className="relative flex items-center justify-center min-h-screen w-screen">
+      <div className="w-[90%] m-auto">
 
-        <h1 className="text-2xl">Informações pessoais</h1>
+        <h1 className="text-3xl font-semibold">Informações pessoais</h1>
         <div className="flex flex-col gap-3">
           <Input title="Digite seu nome" />
           <Input title="WhatsApp: (DD) + Número" />
@@ -35,43 +35,55 @@ export default function Register() {
 
         </div>
 
-        <h1 className="text-2xl">Horário de expediente</h1>
-        <div>
-          <CheckBox isChecked={selectedTime === "manha"} onChange={() => handleChangeTime("manha")} disabled={selectedTime && selectedTime !== "manha"} title="10:00 às 19:00" id="manha" />
-          <CheckBox isChecked={selectedTime === "tarde"} onChange={() => handleChangeTime("tarde")} disabled={selectedTime && selectedTime !== "tarde"} title="13:00 às 22:00" id="tarde" />
-          <CheckBox isChecked={selectedTime === "noite"} onChange={() => handleChangeTime("noite")} disabled={selectedTime && selectedTime !== "noite"} title="22:00 às 00:00" id="noite" />
-        </div>
-
-        <h1 className="text-2xl">Cargo Atual</h1>
-        <div>
-          <div>
-            <CheckBox isChecked={selectedRole === "trial"} onChange={() => handleChangeRole("trial")} disabled={selectedRole && selectedRole !== "trial"} title="Trial (Menor que trê meses)" id="trial" />
-            <CheckBox isChecked={selectedRole === "jri"} onChange={() => handleChangeRole("jri")} disabled={selectedRole && selectedRole !== "jri"} title="JR I" id="jri" />
-            <CheckBox isChecked={selectedRole === "jrii"} onChange={() => handleChangeRole("jrii")} disabled={selectedRole && selectedRole !== "jrii"} title="JR II" id="jrii" />
-            <CheckBox isChecked={selectedRole === "jriii"} onChange={() => handleChangeRole("jriii")} disabled={selectedRole && selectedRole !== "jriii"} title="JR III" id="jriii" />
+        <div className="relative w-2/4">
+          <h1 className="text-3xl font-semibold mt-10">Horário de expediente</h1>
+          <div className="mt-5">
+            <CheckBox isChecked={selectedTime === "manha"} onChange={() => handleChangeTime("manha")} disabled={selectedTime && selectedTime !== "manha"} title="10:00 às 19:00" id="manha" />
+            <CheckBox isChecked={selectedTime === "tarde"} onChange={() => handleChangeTime("tarde")} disabled={selectedTime && selectedTime !== "tarde"} title="13:00 às 22:00" id="tarde" />
+            <CheckBox isChecked={selectedTime === "noite"} onChange={() => handleChangeTime("noite")} disabled={selectedTime && selectedTime !== "noite"} title="22:00 às 00:00" id="noite" />
           </div>
 
-          <div>
-            <CheckBox isChecked={selectedRole === "jriv"} onChange={() => handleChangeRole("jriv")} disabled={selectedRole && selectedRole !== "jriv"} title="JR IV" id="jriv" />
-            <CheckBox isChecked={selectedRole === "pleno"} onChange={() => handleChangeRole("pleno")} disabled={selectedRole && selectedRole !== "pleno"} title="Pleno" id="pleno" />
-            <CheckBox isChecked={selectedRole === "super"} onChange={() => handleChangeRole("super")} disabled={selectedRole && selectedRole !== "super"} title="Super" id="super" />
-            <CheckBox isChecked={selectedRole === "gerente"} onChange={() => handleChangeRole("gerente")} disabled={selectedRole && selectedRole !== "gerente"} title="Gerente" id="gerente" />
+          <h1 className="text-3xl font-semibold mt-4">Cargo Atual</h1>
+          <div className="flex mt-5 justify-between">
+            <div>
+              <CheckBox isChecked={selectedRole === "trial"} onChange={() => handleChangeRole("trial")} disabled={selectedRole && selectedRole !== "trial"} title="Trial (Menor que três meses)" id="trial" />
+              <CheckBox isChecked={selectedRole === "jri"} onChange={() => handleChangeRole("jri")} disabled={selectedRole && selectedRole !== "jri"} title="JR I" id="jri" />
+              <CheckBox isChecked={selectedRole === "jrii"} onChange={() => handleChangeRole("jrii")} disabled={selectedRole && selectedRole !== "jrii"} title="JR II" id="jrii" />
+              <CheckBox isChecked={selectedRole === "jriii"} onChange={() => handleChangeRole("jriii")} disabled={selectedRole && selectedRole !== "jriii"} title="JR III" id="jriii" />
+            </div>
+
+            <div>
+              <CheckBox isChecked={selectedRole === "jriv"} onChange={() => handleChangeRole("jriv")} disabled={selectedRole && selectedRole !== "jriv"} title="JR IV" id="jriv" />
+              <CheckBox isChecked={selectedRole === "pleno"} onChange={() => handleChangeRole("pleno")} disabled={selectedRole && selectedRole !== "pleno"} title="Pleno" id="pleno" />
+              <CheckBox isChecked={selectedRole === "super"} onChange={() => handleChangeRole("super")} disabled={selectedRole && selectedRole !== "super"} title="Super" id="super" />
+              <CheckBox isChecked={selectedRole === "gerente"} onChange={() => handleChangeRole("gerente")} disabled={selectedRole && selectedRole !== "gerente"} title="Gerente" id="gerente" />
+
+            </div>
+
 
           </div>
 
+          <h1 className="text-3xl font-semibold mt-4">Gestor</h1>
+          <div className="flex mt-5 justify-between -mr-8">
+            <div>
+              <CheckBox isChecked={selectedManager === "guto"} onChange={() => handleChangeManager("guto")} disabled={selectedManager && selectedManager !== "guto"} title="Augusto Cezar" id="guto" />
+              <CheckBox isChecked={selectedManager === "greice"} onChange={() => handleChangeManager("greice")} disabled={selectedManager && selectedManager !== "greice"} title="Greice Marques" id="greice" />
+              <CheckBox isChecked={selectedManager === "diogo"} onChange={() => handleChangeManager("diogo")} disabled={selectedManager && selectedManager !== "diogo"} title="Diogo Nunes" id="diogo" />
 
-        </div>
+            </div>
+            <div>
 
-        <h1 className="text-2xl">Gestor</h1>
-        <div>
-          <CheckBox isChecked={selectedManager === "guto"} onChange={() => handleChangeManager("guto")} disabled={selectedManager && selectedManager !== "guto"} title="Augusto Cezar" id="guto" />
-          <CheckBox isChecked={selectedManager === "greice"} onChange={() => handleChangeManager("greice")} disabled={selectedManager && selectedManager !== "greice"} title="Greice Marques" id="greice" />
-          <CheckBox isChecked={selectedManager === "diogo"} onChange={() => handleChangeManager("diogo")} disabled={selectedManager && selectedManager !== "diogo"} title="Diogo Nunes" id="diogo" />
-          <CheckBox isChecked={selectedManager === "duda"} onChange={() => handleChangeManager("duda")} disabled={selectedManager && selectedManager !== "duda"} title="Duda" id="guto" />
-          <CheckBox isChecked={selectedManager === "teteu"} onChange={() => handleChangeManager("teteu")} disabled={selectedManager && selectedManager !== "teteu"} title="Mateus Silva" id="teteu" />
+              <CheckBox isChecked={selectedManager === "duda"} onChange={() => handleChangeManager("duda")} disabled={selectedManager && selectedManager !== "duda"} title="Duda" id="duda" />
+              <CheckBox isChecked={selectedManager === "teteu"} onChange={() => handleChangeManager("teteu")} disabled={selectedManager && selectedManager !== "teteu"} title="Mateus Silva" id="teteu" />
+            </div>
+
+          </div>
         </div>
+        {/* <button type="submit">Cadastrar</button> */}
       </div>
-      {console.log(selectedTime, selectedRole, selectedManager)}
+
+      {/* {console.log(selectedTime, selectedRole, selectedManager)} */}
     </form>
+    
   );
 }
