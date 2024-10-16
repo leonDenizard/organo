4b2e2d21df7e8fd60f544e296ac4b743d032f156
leonDenizard <de.nizard@hotmail.com>
2024-10-16 12:45:13 -0300

    Feat: Iniciado componente profileMenu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import ProfileMenu from "./ProfileMenu";
+
+//const [openMenu, setOpenMenu] = useState(false)
+
+const toggleMenu = () =>{
+
+  
+  console.log("teste")
+}
+
 function Header({ img, name, logout }) {
   return (
     <header className="relative w-full flex justify-end gap-6 items-center top-5">
       <span className="font-medium text-lg">Olá, {name}!</span>
       <div className="relative w-16 rounded-full border-2 border-border-color">
-        <img onClick={logout} src={img} alt="profile photo" className="w-16 rounded-full"/>
+        <img src={img} onClick={toggleMenu} alt="profile photo" className="w-16 rounded-full cursor-pointer"/>
       </div>
+      <ProfileMenu/>
     </header>
   );
 }
---
+++ src/components/ProfileMenu.jsx
@@ -0,0 +1,12 @@
+
+
+export default function ProfileMenu() {
+  return (
+    <div className="absolute top-20 -right-5 bg-card-bg z-10">
+        <ul className="cursor-pointer flex flex-col gap-2 items-center w-28 backdrop-blur-md py-4">
+            <li className="">Editar perfil</li>
+            <li className="">Sair</li>
+        </ul>
+    </div>
+  )
+}
