65068446c398cc963ea2180d0be03e4f28ff76f3
leonDenizard <de.nizard@hotmail.com>
2025-04-07 21:51:22 -0300

    Fix: Corrigido rota post da criação da escala

--- src/services/schedule.js
+++ src/services/schedule.js
@@ -1,44 +1,38 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL
 
 export const uploadScheduleToBD = async (json) => {
   try {
-    const schedule = await fetch(`${API_URL}/api/schedule/`, {
+    const schedule = await fetch(`${API_URL}/schedule/`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
       }
     })
 
+    console.log("API_URL:", API_URL)
+
     const response = await schedule.json()
 
-    console.log(response)
+    console.log("Resposta do GET:", response)
 
-    
-    if(response.length === 0){
-      const uploadSchedule = await fetch(`${API_URL}/api/schedule/`, {
+    if (response.length === 0) {
+      const uploadSchedule = await fetch(`${API_URL}/schedule/`, {
         method: 'POST',
         headers: {
           'Content-type': 'application/json'
         },
         body: JSON.stringify(json)
-       })
+      })
 
-       const response = await uploadSchedule.json()
-
-       if (!response.ok) {
-        console.log(response)
-
+      if (!uploadSchedule.ok) {
+        const errorText = await uploadSchedule.text()
+        console.log("Erro na resposta do servidor:", errorText)
         return
       }
 
-      const data = await response.json()
-      console.log(data)
-    }else{
-      return console.log("Já existe uma escala cadastrada")
-
+    } else {
+      console.log("Já existe uma escala cadastrada")
     }
-   
-
   } catch (error) {
     console.error("Erro ao enviar a escala para o banco de dados: ", error);
     return { success: false, message: `Erro: ${error.message}` };
