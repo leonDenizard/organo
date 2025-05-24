05a9e409ba298fc2f9b90a175e3c073644474a1d
leonDenizard <de.nizard@hotmail.com>
2025-05-24 14:20:02 -0300

    Style: Ajustado resposividade dos filtros na tela dashboard

--- src/components/ButtonFilter.jsx
+++ src/components/ButtonFilter.jsx
@@ -1,6 +1,6 @@
 function ButtonFilter({ name, color, textColor, borderColor, border, onClick}) {
   return (
-    <button onClick={onClick} type="button" className="bg-card-bg font-semibold p-1 px-12 rounded-full
+    <button onClick={onClick} type="button" className="bg-card-bg font-semibold text-nowrap p-1 px-12 md:p-1 md:px-12 rounded-full
     hover:bg-border-color transition duration-300 ease-in-out" style={{ backgroundColor: color, color: textColor, borderColor: borderColor, border: border}}>{name}</button>
   )
 }
--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,15 +1,44 @@
 import ButtonFilter from "./ButtonFilter";
 
-export default function FilterBar({ orderByName, orderByRole, orderByManager, orderByTime, handleSchedule }) {
-
+export default function FilterBar({
+  orderByName,
+  orderByRole,
+  orderByManager,
+  orderByTime,
+  handleSchedule,
+}) {
   return (
-    <div className="flex justify-center gap-10 relative top-14 ">
-        <h4 className="font-bold text-xl tracking-wide">Filtrar por:</h4>
-        <ButtonFilter name={"A-Z"} onClick={orderByName}/>
-        <ButtonFilter name={"Cargo"} onClick={orderByRole}/>
-        <ButtonFilter name={"Horário"} onClick={orderByTime}/>
-        <ButtonFilter name={"Gestor"} onClick={orderByManager}/>
-        <ButtonFilter name={"Escala"} onClick={handleSchedule} color={"rgba(4, 33, 150, 0.3)"} textColor={"rgba(255, 255, 255, 1)"} borderColor={"#193296"} border={"1px solid rgba(0, 51, 255, 1)"}/>
+    <div className="relative top-14">
+      <div
+        className="relative flex w-full xl:justify-center gap-10 overflow-x-scroll md:overflow-x-auto
+        
+        items-center"
+      >
+        <p
+          className="
+          text-nowrap
+          lg:font-bold lg:text-xl lg:tracking-wide"
+        >
+          Filtrar por:
+        </p>
+
+        <ButtonFilter name={"A-Z"} onClick={orderByName} />
+        <ButtonFilter name={"Cargo"} onClick={orderByRole} />
+        <ButtonFilter name={"Horário"} onClick={orderByTime} />
+        <ButtonFilter name={"Gestor"} onClick={orderByManager} />
+        <ButtonFilter
+          name={"Escala"}
+          onClick={handleSchedule}
+          color={"rgba(4, 33, 150, 0.3)"}
+          textColor={"rgba(255, 255, 255, 1)"}
+          borderColor={"#193296"}
+          border={"1px solid rgba(0, 51, 255, 1)"}
+        />
+      </div>
+      <div className="
+      absolute pointer-events-none right-0 top-0 h-full w-16 
+      bg-gradient-to-l from-black/50 to-transparent
+      lg:hidden"></div>
     </div>
-  )
+  );
 }
