970c56d32a5652a2453cef66a4f9c321c41a5da2
leonDenizard <de.nizard@hotmail.com>
2025-09-04 18:00:24 -0300

    feat (globalSchedule): iniciado implementação dde filtros e estilização do layout da escala

--- src/components/GlobalSchedule.jsx
+++ src/components/GlobalSchedule.jsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
+import PopUpMenu from "./PopUpMenu";
+import GlobalScheduleMenu from "./GlobalScheduleMenu";
 
 export default function GlobalSchedule() {
   const API_URL = import.meta.env.VITE_API_URL;
 
   const [dateHeader, setDateHeader] = useState("");
   const [schedule, setSchedule] = useState([]);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [layout, setLayout] = useState("7cols");
+  const [modalType, setModalType] = useState(null);
 
   const fetchSchedule = async () => {
     const getSchedule = await fetch(`${API_URL}/global-schedule`, {
@@ -53,16 +58,62 @@ export default function GlobalSchedule() {
     fetchSchedule();
   }, []);
 
+  const openModal = (type) => {
+    setModalType(type);
+    setIsOpenModal(true);
+  };
+  const closeModal = () => {
+    setIsOpenModal(false);
+    setModalType(null);
+  };
+
+  const getOptions = () => {
+    if (modalType === "layout") return layoutOptions;
+    if (modalType === "user") return userOptions;
+    if (modalType === "date") return dateOptions;
+    return [];
+  };
+
+  const layoutOptions = [
+    { id: "5cols", name: "Escala 5 colunas" },
+    { id: "6cols", name: "Escala 6 colunas" },
+    { id: "7cols", name: "Escala 7 colunas" },
+    { id: "horizontal", name: "Escala Horizontal" },
+  ];
+  const layoutClasses = {
+    "5cols": "grid grid-cols-5 overflow-x-visible gap-1",
+    "6cols": "grid grid-cols-6 overflow-x-visible gap-1",
+    "7cols": "grid grid-cols-7 overflow-x-visible gap-1",
+    "horizontal": "flex flex-row gap-1"
+  };
+
+  const handleSelect = (selected) => {
+    if (modalType === "layout") {
+      console.log("item selecionado", selected);
+      setLayout(selected);
+    }
+    if (modalType === "user") {
+      console.log("Filtro por usuário:", selected);
+    }
+    if (modalType === "date") {
+      console.log("Filtro por data:", selected);
+    }
+    closeModal();
+  };
+
   return (
     <div className="p-12">
-      <h1 className="text-6xl font-bold mb-11">{dateHeader}</h1>
+      <h1 className="text-6xl  font-bold mb-11">{dateHeader}</h1>
 
-      <div className="grid grid-cols-7 overflow-x-visible gap-1">
+      <GlobalScheduleMenu onOpenLayout={() => openModal("layout")} />
+      <div
+        className={layoutClasses[layout] || layoutClasses["7cols"]}
+      >
         {schedule?.map((d) => {
           const [day, month] = d.date.split("-");
           return (
             <div
-              className="text-center text-lg border border-border-color"
+              className="text-center text-lg border border-border-color "
               key={d._id}
             >
               {/* Cabeçalho da data */}
@@ -84,7 +135,7 @@ export default function GlobalSchedule() {
                 {d.shifts?.map((shift) => (
                   <div
                     key={shift._id}
-                    className="border cursor-pointer border-border-color group h-20 hover:brightness-125 transition"
+                    className="border cursor-pointer border-border-color group h-[120px] hover:brightness-125 transition"
                     style={{ backgroundColor: shift.status.color }}
                   >
                     {shift.status.code !== 2 && (
@@ -107,7 +158,9 @@ export default function GlobalSchedule() {
                             transition-all duration-300"
                           style={{ backgroundColor: shift.status.color }}
                         >
-                          <p className="text-gray-400">{shift.status?.description}</p>
+                          <p className="text-gray-400">
+                            {shift.status?.description}
+                          </p>
                         </div>
                         <div
                           className="text-sm px-2 rounded text-gray-300 font-semibold bg-white/5 relative opacity-0 scale-95 
@@ -125,6 +178,13 @@ export default function GlobalSchedule() {
           );
         })}
       </div>
+      {isOpenModal && (
+        <PopUpMenu
+          options={getOptions()}
+          onSelect={handleSelect}
+          closeModal={closeModal}
+        />
+      )}
     </div>
   );
 }
--- src/components/PopUpMenu.jsx
+++ src/components/PopUpMenu.jsx
@@ -4,10 +4,10 @@ export default function PopUpMenu({ options, onSelect, closeModal }) {
   return (
     <div className="fixed w-2/3 m-auto h-4/5 top-0 left-0 right-0 bottom-0 backdrop-blur-3xl z-10 bg-modal-color flex flex-col justify-center items-center border rounded-lg border-border-color">
       <ul className="flex flex-col gap-4">
-        {options.map((option, index) => (
+        {options?.map((option, index) => (
           <li key={index}>
             <button
-              onClick={() => onSelect(option._id || option.startTime)}
+              onClick={() => onSelect(option._id || option.startTime || option.id)}
               className="bg-secundary-color font-semibold text-lg text-start p-3 px-12 rounded-md
                hover:bg-border-color transition duration-300 ease-in-out border  border-border-color min-w-full"
             >
