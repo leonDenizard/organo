453e05b4b8e20fdd33329266686461527b1ebfbd
leonDenizard <de.nizard@hotmail.com>
2025-09-29 15:12:42 -0300

    style (popupchangeschedule): finalizado estilização do componente responável por alterar a escala

--- src/components/PopUpChangeSchedule.jsx
+++ src/components/PopUpChangeSchedule.jsx
@@ -8,17 +8,22 @@ export default function PopUpChangeSchedule({
   shiftId,
   workShifts,
   allStatus,
-  fetchGlobalSchedule
+  fetchGlobalSchedule,
 }) {
   const { updateSchedule } = useGlobalSchedule();
 
-  const [shift, setShift] = useState(shiftId._id || "")
+  const [shift, setShift] = useState(shiftId._id || "");
   const [selectTime, setSelectTime] = useState(shiftId.time._id || "");
   const [selectStatus, setSelectStatus] = useState(shiftId.status._id || "");
 
   // console.log(allStatus);
   // console.log(shiftId);
 
+  const formatDate = (date) => {
+    
+    const dateFormated = date.split("-").join("/")
+    return dateFormated
+  }
   const handleChangeTime = (id) => {
     setSelectTime((prev) => (prev === id ? null : id));
   };
@@ -28,29 +33,23 @@ export default function PopUpChangeSchedule({
   };
 
   const submitUpdateSchedule = async (shiftId, statusId, timeId) => {
-
     try {
-      await updateSchedule(shiftId, statusId, timeId)
+      await updateSchedule(shiftId, statusId, timeId);
 
-      if(fetchGlobalSchedule){
-        await fetchGlobalSchedule()
+      if (fetchGlobalSchedule) {
+        await fetchGlobalSchedule();
       }
 
-      closeModal()
-      
+      closeModal();
     } catch (error) {
-      console.log("Erro ao atualizar escala")
+      console.log("Erro ao atualizar escala");
     }
   };
 
-  useEffect(() => {
-    console.log("ID Selecionado do popUp selectTime:", selectTime);
-    console.log("ID Selecionado do popUp selectStatus:", selectStatus);
-    console.log(date);
-  }, [selectTime, selectStatus]);
+  
 
   return (
-    <div className="fixed w-2/3 m-auto h-4/5 top-0 left-0 right-0 bottom-0 backdrop-blur-3xl z-10 bg-modal-color flex flex-col justify-center items-center border rounded-lg border-border-color">
+    <div className="fixed 2xl:w-2/3 m-auto 2xl:h-4/5 top-0 left-0 right-0 bottom-0 backdrop-blur-3xl z-10 bg-modal-color flex flex-col justify-center items-center border rounded-lg border-border-color">
       <button
         onClick={closeModal}
         className="absolute top-0 right-3 mt-4 px-4 py-2 bg-red-500 text-white rounded"
@@ -58,44 +57,55 @@ export default function PopUpChangeSchedule({
         Fechar
       </button>
 
-      <div className="border">
-        <p>{date}</p>
-        <img src={shiftId.userId?.photoUrl} alt="" />
-        <p>{shiftId.userId?.name}</p>
-        <p>
-          {shiftId.time?.startTime} - {shiftId.time?.endTime}
-        </p>
-        {/* <p>Shift _id {shiftId._id}</p> */}
-      </div>
-
-      <div>
-        <p>Mudar horario</p>
-        {workShifts.map((ws) => (
-          <CheckBox
-            key={ws._id}
-            id={ws._id}
-            title={`${ws.startTime} às ${ws.endTime}`}
-            isChecked={selectTime === ws._id}
-            onChange={() => handleChangeTime(ws._id)}
+      <div className=" 2xl:w-[80%] w-[60%] mb-8">
+        <p className="text-5xl mb-8 ">{formatDate(date)}</p>
+        <div className="flex items-center gap-5">
+          <img
+            className="rounded-full h-20 w-20"
+            src={shiftId.userId?.photoUrl}
+            alt=""
           />
-        ))}
+          <div>
+            <p className="text-2xl font-semibold tracking-wider">{shiftId.userId?.name}</p>
+            <p className="text-gray-400">
+              {shiftId.time?.startTime} - {shiftId.time?.endTime}
+            </p>
+          </div>
+
+          {/* <p>Shift _id {shiftId._id}</p> */}
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2 2xl:w-[80%] w-[60%]">
+        <div className="">
+          <p className="text-2xl text-gray-200 mb-3">Mudar horario</p>
+          {workShifts.map((ws) => (
+            <CheckBox
+              key={ws._id}
+              id={ws._id}
+              title={`${ws.startTime} às ${ws.endTime}`}
+              isChecked={selectTime === ws._id}
+              onChange={() => handleChangeTime(ws._id)}
+            />
+          ))}
+        </div>
+
+        <div>
+          <p className="text-2xl text-gray-200 mb-3">Mudar de status</p>
+          {allStatus.map((st) => (
+            <CheckBox
+              key={st._id}
+              id={st._id}
+              title={st.description}
+              isChecked={selectStatus === st._id}
+              onChange={() => handleChangeStatus(st._id)}
+            />
+          ))}
+        </div>
       </div>
 
       <div>
-        <p>Mudar de status</p>
-        {allStatus.map((st) => (
-          <CheckBox
-            key={st._id}
-            id={st._id}
-            title={st.description}
-            isChecked={selectStatus === st._id}
-            onChange={() => handleChangeStatus(st._id)}
-          />
-        ))}
-      </div>
-
-      <div>
-        <button
+        <button className="border-2 border-border-color p-2 w-[350px] hover:bg-white/5 rounded-md hover:transition-all mt-8"
           onClick={() => submitUpdateSchedule(shift, selectStatus, selectTime)}
         >
           Atualizar escala
