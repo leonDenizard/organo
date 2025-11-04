5ff574c68284235fa0d05341c2bc147d59c11b30
leonDenizard <de.nizard@hotmail.com>
2025-11-03 21:21:39 -0300

    setup: inserido componente Analytics para metrificar o acesso ao app

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import AuthProvider from './context/AuthProvider.jsx'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
